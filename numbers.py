70e71aa324fcd4ad2ed546deecff1b5a88c44800
Ervin Pepic <ervin@Ervins-MacBook-Pro-2.local>
2019-04-05 00:24:52 +0200

    Working with numbers

---
+++ numbers.py
@@ -0,0 +1,40 @@
+num_int = 3 
+num_float = 3.3
+
+print(type(num_int), type(num_float))
+
+#arithmetic operations:
+
+print(3 + 3) #addidion
+print(3 - 1)
+print(3 / 1)
+print(3 * 3)
+print(3 % 2) #reminders
+
+print(3 * (2 + 1))
+
+num1 = 1
+
+num1 += 1 #also is available with other operators
+print(num1)
+
+print(abs(-3))
+print(round(3.231123, 2))
+
+###comparison
+
+num_comp1 = 3
+num_comp2 = 1
+print(num_comp1 == num_comp2)
+print(num_comp1 != num_comp2)
+print(num_comp1 > num_comp2)
+print(num_comp1 < num_comp2)
+print(num_comp1 <= num_comp2)
+print(num_comp1 >= num_comp2)
+
+num_str = '100'
+num_str2 = '1200'
+print(num_str + num_str2)
+
+num_str = int(num_str)
+num_str = int(num_str2)
